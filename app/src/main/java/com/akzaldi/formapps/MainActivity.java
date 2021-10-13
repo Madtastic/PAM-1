79270a8a1230f406a167a4b9b5a82d98d5396429
Akhmad Rizaldi <92414363+Madtastic@users.noreply.github.com>
2021-10-13 18:24:46 +0700

    Add files via upload

---
+++ app/src/main/java/com/akzaldi/formapps/MainActivity.java
@@ -0,0 +1,33 @@
+package com.akzaldi.formapps;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText inputName = (EditText) findViewById(R.id.id_input_name);
+        EditText inputEmail = (EditText) findViewById(R.id.id_input_email);
+        EditText inputPhone = (EditText) findViewById(R.id.id_input_phone);
+        EditText inputPassword = (EditText) findViewById(R.id.id_input_password);
+
+        Button btnSave = findViewById(R.id.button);
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("--Nama--", inputName.getText().toString());
+                Log.d("--Email--", inputEmail.getText().toString());
+                Log.d("--Phone--", inputPhone.getText().toString());
+                Log.d("--Password--", inputPassword.getText().toString());
+            }
+        });
+    }
+}
